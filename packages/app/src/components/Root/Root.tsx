5f191ee7a20705c83ccc4f9f201c22f3ff0ba208
kyo <90518589+aneomagig@users.noreply.github.com>
2025-08-28 19:40:19 +0900

    feat(app): wire api-usage plugin route and sidebar

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -28,6 +28,7 @@ import SearchIcon from '@material-ui/icons/Search';
 import { MyGroupsSidebarItem } from '@backstage/plugin-org';
 import GroupIcon from '@material-ui/icons/People';
 import { NotificationsSidebarItem } from '@backstage/plugin-notifications';
+import TimelinIcon from '@material-ui/icons/Timeline';
 
 const useSidebarLogoStyles = makeStyles({
   root: {
@@ -76,6 +77,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+        <SidebarItem icon={TimelinIcon} to="api-usage" text="API Usage" />
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
